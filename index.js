181fe2281d8a90707187c3722b0e204427c11acd
unknown <madhavasurya99@gmail.com>
2024-02-14 09:04:24 +0530

    Intial commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 const path=require('path');
-const port=8143;
+const port=8144;
 const list=require('./models/schema');
 const db=require('./config/mongoose');
 app.set('views', path.join(__dirname, 'views'));
